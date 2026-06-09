b1b97ca991f57c3cbd8ae5cefb2ae532364aa774
dhanya-agile <dhanya-agile@gmail.com>
2026-06-09 15:45:28 +0530

    Updated folder AXI_PackageNew1

---
+++ AXI_PackageNew1/axi_dependent_tables.sql
@@ -0,0 +1,19 @@
+<<
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+>>
+
+--This may not be needed
+DROP TABLE Axi_UserFavourites
+
+
+<<
+CREATE TABLE Axi_UserFavourites (
+    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    UserName VARCHAR(255) NOT NULL,
+    CommandText TEXT NOT NULL,
+    TargetURL VARCHAR(4000) NOT NULL,
+    FavOrder INT,
+    CreatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uq_user_command UNIQUE (UserName, CommandText)
+)
+>>
