6ab7e72261bd27637f66f2978ec1f3ad16f7132d
dhanya-agile <agile.dhanya@gmail.com>
2026-07-20 15:10:50 +0530

    Add files via upload

---
+++ TestPackageNew/Structures/Postgre/Scripts/Test_Script.sql
@@ -0,0 +1,11 @@
+<<
+CREATE TABLE datatest (UserID INT PRIMARY KEY,FullName VARCHAR(100),Email VARCHAR(100))
+>>
+
+<<
+CREATE TABLE Purchasetest (ProductID INT PRIMARY KEY,ProductName VARCHAR(50),Price DECIMAL(10, 2),StockQuantity INT)
+>>
+
+
+
+
